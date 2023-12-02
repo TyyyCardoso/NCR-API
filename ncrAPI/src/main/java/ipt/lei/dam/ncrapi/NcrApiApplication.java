b8aae030f0e076e002fc4ff70c0430129615e4fe
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-02 19:16:54 +0000

    Fix #2 To try to deploy at the server

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/NcrApiApplication.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/NcrApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NcrApiApplication {
+public class NcrApiApplication extends ServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(NcrApiApplication.class, args);

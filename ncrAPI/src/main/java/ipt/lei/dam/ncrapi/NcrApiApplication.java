28795020ed836327a542af050b44944aed214948
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-03 03:14:58 +0000

    #4 Fix on servlets

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/NcrApiApplication.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/NcrApiApplication.java
@@ -2,8 +2,9 @@ package ipt.lei.dam.ncrapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class NcrApiApplication extends ServletInitializer{
 
     public static void main(String[] args) {

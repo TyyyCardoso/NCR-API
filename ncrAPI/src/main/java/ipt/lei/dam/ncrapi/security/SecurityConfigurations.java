4e59cbeea1a93728c2a0d0e4159e105c64f1cb12
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-03 03:05:33 +0000

    #2 Update on the security servlet

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/security/SecurityConfigurations.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/security/SecurityConfigurations.java
@@ -36,7 +36,8 @@ public class  SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .requestMatchers(antMatcher("/security-servlet/*")).permitAll()
+                        .requestMatchers(mvc.pattern("/auth/**")).permitAll()
                         //.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         //.requestMatchers(HttpMethod.POST, "/auth/recover").permitAll()
                         //.requestMatchers(HttpMethod.POST, "/auth/login").hasRole("MEMBER")
@@ -47,22 +48,7 @@ public class  SecurityConfigurations {
                 .build();
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
-                        //.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        //.requestMatchers(HttpMethod.POST, "/auth/recover").permitAll()
-                        //.requestMatchers(HttpMethod.POST, "/auth/login").hasRole("MEMBER")
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
 
-    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{

60749e50813668aa147854149df1d56dbb8e2c8f
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-03 03:18:14 +0000

    #5 Fix On error double servlet

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/NcrApiApplication.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/NcrApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class NcrApiApplication extends ServletInitializer{
+@SpringBootApplication
+public class NcrApiApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(NcrApiApplication.class, args);

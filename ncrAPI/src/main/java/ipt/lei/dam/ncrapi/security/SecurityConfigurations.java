8b25efa9a5ab2403e906a3b1e80ffbc846c22eff
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-03 02:59:49 +0000

    Update security config so it can be used with tomcat

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/security/SecurityConfigurations.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/security/SecurityConfigurations.java
@@ -13,13 +13,39 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigurations {
+public class  SecurityConfigurations {
 
     @Autowired
     SecurityFilter securityFilter;
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    SecurityFilterChain appSecurity(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        //.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        //.requestMatchers(HttpMethod.POST, "/auth/recover").permitAll()
+                        //.requestMatchers(HttpMethod.POST, "/auth/login").hasRole("MEMBER")
+                        .anyRequest().authenticated()
+
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception{

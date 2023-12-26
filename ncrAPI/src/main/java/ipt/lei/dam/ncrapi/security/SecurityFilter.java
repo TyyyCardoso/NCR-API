3930aeb7fe74a642e6e6d230940fa61fee815031
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-26 22:45:12 +0000

    Adicionado logs para despiste do erro

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/security/SecurityFilter.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package ipt.lei.dam.ncrapi.security;
 
+import ipt.lei.dam.ncrapi.LoggingInterceptor;
 import ipt.lei.dam.ncrapi.database.repos.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,12 +25,17 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     UserRepository userRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingInterceptor.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = this.recoverToken(request);
         if(token != null){
+            logger.info("LOGGED TOKEN: " + token);
             String email = tokenService.validateToken(token);
+            logger.info("LOGGED EMAIL: " + email);
             UserDetails user = userRepository.getUserByEmail(email);
+            logger.info("LOGGED USER: " + user.getUsername());
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null , user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);

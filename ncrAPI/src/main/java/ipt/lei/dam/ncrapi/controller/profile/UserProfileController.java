5bf3d3993307e62f00daff1e82a6c1ec26aab2c2
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2024-01-10 18:04:47 +0000

    Fix update profile

--- ncrAPI/src/main/java/ipt/lei/dam/ncrapi/controller/profile/UserProfileController.java
+++ ncrAPI/src/main/java/ipt/lei/dam/ncrapi/controller/profile/UserProfileController.java
@@ -71,7 +71,7 @@ public class UserProfileController {
 
             userService.save(user);
             
-            if(user.getImage() != null && !user.getImage().isEmpty()){
+            if(null != user.getImage() && !user.getImage().isEmpty()){
                 System.out.println("returning with image name: " + user.getImage());
                 return ResponseEntity.ok().body(new DefaultResponseDTO(200, user.getImage()));
             }
